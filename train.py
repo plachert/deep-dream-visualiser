a242e554df91f5711fc4a516b9aef75403004251
plachert <pmlachert@gmail.com>
2023-03-28 18:35:33 +0200

    Refactor, add logger and checkpoint

--- train.py
+++ train.py
@@ -4,13 +4,26 @@ import pytorch_lightning as pl
 
 
 def main():
-    train_ds = MNISTDataset(train=True)
-    test_ds = MNISTDataset(train=False)
+    train_ds = MNISTDataset(train=True, corrupt=False)
+    test_ds = MNISTDataset(train=False, corrupt=False)
     
-    datamodule = MNISTDataModule(train_ds, test_ds)
+    datamodule = MNISTDataModule(train_ds, test_ds, batch_size=256)
     model = DenseMNISTNet()
     
-    trainer = pl.Trainer(max_epochs=5)
+    experiment_path = "models/dense_mnist"
+    
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        monitor="val/loss",
+        dirpath=f"{experiment_path}",
+        filename="best",
+        mode="min",
+    )
+    
+    trainer = pl.Trainer(
+        max_epochs=50,
+        logger=pl.loggers.TensorBoardLogger(f"{experiment_path}/log"),
+        callbacks=[checkpoint_callback]
+        )
     
     trainer.fit(model, datamodule)
     
