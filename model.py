0372b46ffcb41049332a004809aa3b6c3b629eb0
plachert <pmlachert@gmail.com>
2023-05-11 15:21:19 +0200

    Refactor

--- model.py
+++ model.py
@@ -1,40 +1,66 @@
-import torch
 import torch.nn as nn
-import pytorch_lightning as pl
+from torchvision import models
+from typing import List
 
 
-class DenseMNISTNet(pl.LightningModule):
-    def __init__(self):
+def flatten_modules(module):
+    flattened_modules = []
+    for child in module.children():
+        if isinstance(child, nn.Sequential):
+            flattened_modules.extend(flatten_modules(child))
+        else:
+            flattened_modules.append(child)
+    return flattened_modules
+
+
+class ModelWithActivations(nn.Module):
+    def __init__(self, model: nn.Module) -> None:
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(784, 32), # 28*28
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, 10),
-        )
-
-    def forward(self, image):
-        pred = self.net(image)
-        return pred
-
-    def training_step(self, batch, batch_idx):
-        image, digit = batch
-        pred = self(image)
-        digit = digit.type_as(pred)
-        loss = torch.nn.functional.cross_entropy(pred, digit)
-        self.log("train/loss", loss, prog_bar=True)
-        return loss
+        self.model = model
+        self.model.eval()
+        for param in self.model.parameters():
+            param.requires_grad = False 
+        self._activations = []
+        self._register_activation_hook()
+        
+    def get_target_activation(self, target_idx):
+        """Return activation of the neuron associated with the target."""
+        if not self._activations: # TODO: maybe some warning if the forward pass hasn't been called?
+            return self._activations
+        return [self._activations[-1][:, target_idx]] # list for consistency with other methods
+        
+    def get_activations_by_idx(self, idxs: List[int]):
+        """Return activations by indices."""
+        if not self._activations: # TODO: maybe some warning if the forward pass hasn't been called?
+            return self._activations
+        return [self._activations[idx][1] for idx in idxs]
     
-    def validation_step(self, batch, batch_idx):
-        image, digit = batch
-        pred = self(image)
-        digit = digit.type_as(pred)
-        loss = torch.nn.functional.cross_entropy(pred, digit)
-        self.log("val/loss", loss, prog_bar=True)
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+    def get_activations_by_types(self, types: List[str]):
+        """Return all activations of layers of given types e.g. ReLU."""
+        if not self._activations:
+            return self._activations # TODO: maybe some warning if the forward pass hasn't been called?
+        return [activation[1] for activation in self._activations if activation[0] in types]
+    
+    def get_all_activations(self):
+        """Return activations of all layers."""
+        idxs = list(range(0, len(self._activations)))
+        return self.get_activations_by_idx(idxs)
+        
+    
+    def _register_activation_hook(self):
+        def activation_hook(module, input_, output):
+            self._activations.append((module.__class__.__name__, output))
+        for layer in flatten_modules(self.model):
+            layer.register_forward_hook(activation_hook)
+    
+    def forward(self, input_):
+        self._activations.clear()
+        return self.model.forward(input_)
+            
+            
+if __name__ == "__main__":
+    import torch
+    deep_dream = DeepDreamModel(model=models.vgg16(pretrained=True))
+    print(len(deep_dream.get_activations_by_types(["Linear"])))
+    deep_dream(torch.rand(1, 3, 100, 100))
+    # print(len(deep_dream.get_activations_by_types(["Linear"])))
