55122edc0fa12ff92125dcb1c2d7cbd3583ea83d
plachert <pmlachert@gmail.com>
2023-05-22 12:40:25 +0200

    Preinit activations, add list_all_available_parameters and strategy_parameters property

--- deepdream/model.py
+++ deepdream/model.py
@@ -13,6 +13,10 @@ class ActivationFilter:
     """Abstract class for filtering strategies."""
     def filter_activations(self, activations: List[Tuple[str, torch.Tensor]]) -> List[Tuple[str, torch.Tensor]]:
         raise NotImplementedError
+    
+    def list_all_available_parameters(self, activations: List[Tuple[str, torch.Tensor]]) -> List:
+        """List all available options based on the strategy."""
+        raise NotImplementedError
 
 
 @register_filter
@@ -23,6 +27,9 @@ class TypeActivationFilter(ActivationFilter):
     
     def filter_activations(self, activations: List[Tuple[str, torch.Tensor]]) -> List[Tuple[str, torch.Tensor]]:
         return [activation for activation in activations if activation[0] in self.types]
+    
+    def list_all_available_parameters(self, activations: List[Tuple[str, torch.Tensor]]) -> List:
+        return list(set([activation[0] for activation in activations]))
 
 
 @register_filter
@@ -34,6 +41,9 @@ class IndexActivationFilter(ActivationFilter):
     def filter_activations(self, activations: List[Tuple[str, torch.Tensor]]) -> List[Tuple[str, torch.Tensor]]:
         return [activations[idx] for idx in self.indices]
     
+    def list_all_available_parameters(self, activations: List[Tuple[str, torch.Tensor]]) -> List:
+        return list(range(len(activations)))
+    
 
 @register_filter 
 class TargetsActivationFitler(ActivationFilter):
@@ -42,8 +52,12 @@ class TargetsActivationFitler(ActivationFilter):
         self.indices = indices
     
     def filter_activations(self, activations: List[Tuple[str, torch.Tensor]]) -> List[Tuple[str, torch.Tensor]]:
-        last_activations = activations[-1][1] # last layer
-        return [(f"target_{idx}", last_activations[:, idx]) for idx in self.indices]
+        last_activation = activations[-1][1] # last layer
+        return [(f"target_{idx}", last_activation[:, idx]) for idx in self.indices]
+    
+    def list_all_available_parameters(self, activations: List[Tuple[str, torch.Tensor]]) -> List:
+        last_activation = activations[-1][1]
+        return list(range(last_activation.shape[-1]))
 
 
 class ModelWithActivations(nn.Module):
@@ -56,13 +70,16 @@ class ModelWithActivations(nn.Module):
         self._activations = []
         self._register_activation_hook()
         self.activation_filter = activation_filter
+        
+    @property
+    def strategy_parameters(self):
+        if self.activation_filter is None:
+            return None
+        return self.activation_filter.list_all_available_parameters(self._activations)
     
     @property
     def activations(self) -> List[torch.Tensor]:
         """Return activations based on the activation_filter."""
-        if not self._activations:
-            # Raise a warning if the forward pass hasn't been called
-            print("Warning: No activations available. Perform a forward pass first.")
         filtered_activations = self._activations
         if self.activation_filter:
             filtered_activations = self.activation_filter.filter_activations(self._activations)
@@ -73,6 +90,7 @@ class ModelWithActivations(nn.Module):
         def activation_hook(module, input_, output):
             self._activations.append((module.__class__.__name__, output))
         for layer in flatten_modules(self.model):
+            activation_hook(layer, None, None)
             layer.register_forward_hook(activation_hook)
     
     def forward(self, input_):
