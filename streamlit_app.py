f60d8dd609fc50d0c444327c3491c92707fa39fb
plachert <pmlachert@gmail.com>
2023-06-04 18:29:53 +0200

    Add run deepdream

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+import time
+
 import streamlit as st
 
 from app import get_strategy_params
+from app import run_deepdream
 from deepdream.config import SUPPORTED_CONFIGS
+from deepdream.image_processing import channel_last
+from deepdream.image_processing import convert_to_255scale
 from deepdream.model import SUPPORTED_FILTERS
 
 st.sidebar.title('Deep Dream Visualiser')
@@ -35,6 +40,31 @@ with deepdream_table:
         'Regularization coeff', 0., 1., 0.1, 0.05,
     )
     lr = st.number_input('Learning Rate', 0.001, 1., 0.01, 0.001)
+
+
+images = run_deepdream(
+    image_path=None,
+    config_name=model_selection,
+    strategy_name=strategy_selection,
+    strategy_params=strategy_params,
+    jitter_size=jitter_size,
+    octave_n=octave_n,
+    octave_scale=octave_scale,
+    n_iterations=n_iterations,
+    regularization_coeff=regularization_coeff,
+    lr=lr,
+)
+
+placeholder = st.empty()
+while True:
+    for i, image in enumerate(images):
+        print(image.shape)
+        placeholder.image(
+            channel_last(
+                convert_to_255scale(image),
+            ), f'iter {i}',
+        )
+        time.sleep(1)
 
 # from PIL import Image
 # import numpy as np
